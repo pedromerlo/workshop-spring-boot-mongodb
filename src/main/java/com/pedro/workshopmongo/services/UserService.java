0513574ed34fb88afc13a53e6436404cd3e42e0b
pedromerlo <pedro.merlo.junior@gmail.com>
2024-07-20 16:02:02 -0300

    Atualização de usuário com PUT

--- src/main/java/com/pedro/workshopmongo/services/UserService.java
+++ src/main/java/com/pedro/workshopmongo/services/UserService.java
@@ -21,23 +21,33 @@ public class UserService {
         return repo.findAll();
     }
 
-    public User findById(String id){
+    public User findById(String id) {
         Optional<User> obj = repo.findById(id);
-        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
-    public User insert(User obj){
+    public User insert(User obj) {
         return repo.insert(obj);
     }
 
-
-    public void delete(String id){
+    public void delete(String id) {
         findById(id);
         repo.deleteById(id);
     }
 
-    /*Implemntçao DTO feita na classe service para aproveitar acesso ao database */
-    public User fromDTO(UserDTO objDto){
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    /* Implemntçao DTO feita na classe service para aproveitar acesso ao database */
+    public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
