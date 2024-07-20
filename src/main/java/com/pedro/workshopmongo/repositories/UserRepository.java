eb345707f849e710c7d0afdd5c7d2ca29fa5e2de
pedromerlo <pedro.merlo.junior@gmail.com>
2024-07-20 08:32:46 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/pedro/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.pedro.workshopmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pedro.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
