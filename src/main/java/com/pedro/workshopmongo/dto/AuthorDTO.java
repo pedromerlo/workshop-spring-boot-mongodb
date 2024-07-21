064dc7aa9a06ecf1dd585670575a0242a828bcde
pedromerlo <pedro.merlo.junior@gmail.com>
2024-07-21 17:02:22 -0300

    Referenciando os posts do usuário

--- src/main/java/com/pedro/workshopmongo/dto/AuthorDTO.java
+++ src/main/java/com/pedro/workshopmongo/dto/AuthorDTO.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import com.pedro.workshopmongo.domain.User;
 
 public class AuthorDTO implements Serializable{
-    private static final Long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
     
     private String id;
     private String name;
